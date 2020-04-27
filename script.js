fa6cd619e464f7702136540ec58f503c9ba1bf32
Andre de Mendonça Maia <60257960+andre4130@users.noreply.github.com>
2020-04-27 10:51:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
   $('#getjoke').on('click',function (){
   var $chiste = $('#chiste')
    $('#chiste').html('');
@@ -11,4 +11,31 @@ $(document).ready(function () {
       console.log(data.value.joke)
         $chiste.append(data.value.joke);
       }});
-})});
+})});*/
+
+$(document).ready(function() {
+	  
+  function get_joke(){
+	var randomArray = ['random1.jpg', 'random2.jpg', 'random3.jpg', 'random4.jpg'];
+	var path = 'images/';
+	var $chiste = $('#chiste')
+	$.ajax({
+		type:'GET',
+		url: 'http://api.icndb.com/jokes/random',
+		success: function(data){
+		  console.log(data.value.joke)
+		  var random = randomArray[Math.floor(Math.random() * randomArray.length)];
+		  console.log(random);
+		  
+		  $('#body').css({'background-image':'url('+path+random+')'});
+		  $('#chiste').html(data.value.joke);
+		}
+	});
+  }
+  
+  $('#getjoke').on('click',function (){
+	get_joke();
+  })
+  
+  
+});
