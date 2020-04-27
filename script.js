f47f89256f799fef8caf12757eeb99814be8ce50
Andre de Mendonça Maia <60257960+andre4130@users.noreply.github.com>
2020-04-27 10:54:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*$(document).ready(function () {
-  $('#getjoke').on('click',function (){
-  var $chiste = $('#chiste')
-   $('#chiste').html('');
-  $("#body").removeClass("body1");
-  $("#body").addClass("body2");
-  $.ajax({
-    type:'GET',
-    url: 'http://api.icndb.com/jokes/random',
-    success: function(data){
-      console.log(data.value.joke)
-        $chiste.append(data.value.joke);
-      }});
-})});*/
-
 $(document).ready(function() {
 	  
   function get_joke(){
